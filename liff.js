62d13c4587b859965756f9ba36da3a93fb6adf6c
hoatruong <hoatruongdev09@gmail.com>
2025-02-28 14:14:36 +0700

    update line init

--- liff.js
+++ liff.js
@@ -1,14 +1,8 @@
-const initLiff = async () => {
-    try {
-        await liff.init({ liffId: '2006981835-7GgjW3jm' });
-        if (!liff.isLoggedIn()) {
-            liff.login();
-        }
-        console.log(liff.getIdToken())
-        document.getElementById("user-id").innerHTML(liff.getIdToken())
-    } catch (err) {
-        console.error(err)
-    }
-}
-
-initLiff()
+// const initLiff = async () => {
+//     await liff.init({ liffId: '2006981835-7GgjW3jm' })
+//     if (!liff.isLoggedIn()) {
+//         await liff.login();
+//     }
+//     console.log(liff.getIdToken())
+// }
+// initLiff()
